abe35e37083a02989afc682558b58e8a3ee183c4
ViniQuadros <viniciusquadrosvq@gmail.com>
2024-07-15 20:45:22 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-inner .slide');
+
+function showSlide(index) {
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+// Inicializa o carrossel com o primeiro slide
+showSlide(currentSlide);
